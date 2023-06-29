f0ac2cbedfa4d6436f6ec9a287df64ac27e8ba85
shreyansh <shreyansh.brbd@gmail.com>
2023-06-29 14:10:47 +0530

    updated readme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   const [user] = useAuthState(auth);
   return (
     <div className={style.nav}>
-      <h1 className={style.heading}>ChitChat</h1>
+      <h1 className={style.heading}>SociaBay</h1>
       {user ? <SignOut /> : <SignIn />}
     </div>
   );
